8e73fc8e6faf3f44084e78c873b68758e48389e5
alok9693 <96759882+alok9693@users.noreply.github.com>
2022-01-25 18:39:14 +0530

    Add files via upload

---
+++ output2.py
@@ -0,0 +1,60 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: C:/Users/SS/Desktop/run.py =====================
+Que1
+Python is a case sensitive language
+
+Part a
+length of the string is 35
+
+Part b
+The string in reverse order is: egaugnal evitisnes esac a si nohtyP
+
+Part c
+a case sensitive 
+
+Part d
+After replacing in input string: Python is object oriented language
+After replacing in new string: object oriented 
+
+Part e
+10
+
+Part f
+Pythonisacasesensitivelanguage
+
+
+Hey,alok kumar vishwakarma Here!
+My SID is 21104127
+I am from EE department and my CGPA is 9.5
+Que3
+a= 56
+b= 10
+
+Part a
+a&b: 8
+
+Part b
+a|b: 58
+
+Part c
+a^b: 50
+
+Part d
+Left shifting both a and b with 2 bits: 224 40
+
+Part e
+Right shifting a with 2 bits and b with 4 bits: 14 0
+
+
+Give a number:39
+Give a number:43
+Give a number:21
+43 is greatest number
+Give a word:alok
+No
+Give a number:8
+Give a number:2
+Give a number:4
+The triangle is not possible
